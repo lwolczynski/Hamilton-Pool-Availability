e284ed5f64f64ac53f48f0f3d079008083a9ca7e
lwolczynski <54366429+lwolczynski@users.noreply.github.com>
2019-09-17 10:55:55 -0500

    Update _config.py

--- _config.py
+++ _config.py
@@ -4,7 +4,7 @@ sender_email = '' #gmail sender address
 password = '' #gmail sender password
 receiver_email = '' #receiver email
 
-path = '/home/user/hamilton_pool_availability'
+path = '/home/user/hamilton_pool_availability' #path to keep files
 
 #dates to check
 dates = ['08-31', '09-01', '09-02', '09-07', '09-08', '09-14', '09-15', '09-21', '09-22', '09-28', '09-29', '10-05', '10-06', '10-12', '10-13']
